85374cfd077cf162b8df69b8685484eacee037e5
Victor Eno <victor_eno345@protonmail.com>
2023-07-10 18:09:00 +0100

    testing changes

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
-import Toggle from "../components/Toggle";
 import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 
 
